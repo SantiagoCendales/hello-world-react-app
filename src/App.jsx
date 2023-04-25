25e9bb842c6cf65218e6d63eb33bf5f189759423
Santiago Cendales <srcendales94@gmail.com>
2023-04-25 18:13:04 -0500

    Change fixed data for data array

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,39 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 export const App = () => {
   const format = (username) => `@${username}`
 
+  const users = [
+    {
+      userName: 'srcendales',
+      name: 'Santiago Cendales',
+      isFollowing: true
+    },
+    {
+      name: 'Julian DuranS',
+      isFollowing: false
+    },
+    {
+      userName: 'elonmusk',
+      name: 'Elon Musk',
+      isFollowing: true
+    }
+  ]
+
   return (
     <section className='App'>
-      <TwitterFollowCard formatUsername={format} userName='elonmusk' name="Elon Musk" isFollow />
+      {
+        users.map(user => (
+          <TwitterFollowCard 
+            key={user.userName}
+            formatUsername={format}
+            userName={user.userName}
+            name={user.name}
+            isFollow={user.isFollowing}
+          />
+        ))
+      }
+      {/* <TwitterFollowCard formatUsername={format} userName='elonmusk' name="Elon Musk" isFollow />
       <TwitterFollowCard formatUsername={format} userName='srcendales' name="Santiago Cendales" />
-      <TwitterFollowCard formatUsername={format} name="Andres Duran"/>
+      <TwitterFollowCard formatUsername={format} name="Andres Duran"/> */}
     </section>
   )
 }
